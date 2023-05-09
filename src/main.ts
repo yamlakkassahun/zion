3f31c6d0427363a782caf8045f664dc753f837a6
Yamlak Kassahun <yamlak.k@gmail.com>
2023-05-09 14:10:38 +0300

    add swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,15 @@ async function bootstrap() {
   //set up swagger docs
   const config = new DocumentBuilder()
     .setTitle('NestJs Docker Server')
-    .setDescription('The cats API description')
+    .setDescription('The API description')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
-  const document = SwaggerModule.createDocument(app, config); 
-  SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   //server is leasing on port 3000
-  await app.listen(3000, async () => { 
-    console.log(`server is running on :${ await app.getUrl()}`)
+  await app.listen(3000, async () => {
+    console.log(`server is running on :${await app.getUrl()}`);
   });
 }
 bootstrap();
-
